f65bc610d6d32e083c1d9eecc58a08194a4c8077
Jimmy Källhagen <TheEnchantedPotato@proton.me>
2026-03-21 18:31:37 +0100

    Update nordix-cli-snapshot.sh

--- nordix-cli-snapshot.sh
+++ nordix-cli-snapshot.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# ============================================
-# Nordix CLI Snapshot
-# ZFS Snapshot Management for Nordix
-# "Nordix follows the law of performance"
-# ============================================
+##=============================================================================##
+ # SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0                       #
+ # Nordix license - https://polyformproject.org/licenses/noncommercial/1.0.0   #
+ # Copyright (c) 2025 Jimmy Källhagen                                          #
+ # Part of Nordix - https://github.com/jimmykallhagen/Nordix                   #
+ # Nordix and Yggdrasil are trademarks of Jimmy Källhagen                      #
+##=============================================================================##
 
 # ============================================
 # NORDIX COLOR THEME
