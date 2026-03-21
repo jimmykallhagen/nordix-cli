ce977e6ea870837909aa7f0d321122b169e9b061
Jimmy Källhagen <TheEnchantedPotato@proton.me>
2026-03-21 18:30:35 +0100

    Update nordix-cli-image.sh

--- nordix-cli-image.sh
+++ nordix-cli-image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-##=========================================================##
- # SPDX-License-Identifier: GPL-3.0-or-later               #
- # Copyright (c) 2025 Jimmy Källhagen                      #
- # Part of Yggdrasil - Nordix desktop environment          #
- # Nordix and Yggdrasil are trademarks of Jimmy Källhagen  #
-##=========================================================##
+##=============================================================================##
+ # SPDX-License-Identifier: PolyForm-Noncommercial-1.0.0                       #
+ # Nordix license - https://polyformproject.org/licenses/noncommercial/1.0.0   #
+ # Copyright (c) 2025 Jimmy Källhagen                                          #
+ # Part of Nordix - https://github.com/jimmykallhagen/Nordix                   #
+ # Nordix and Yggdrasil are trademarks of Jimmy Källhagen                      #
+##=============================================================================##
+
 # ============================================
 # Nordix CLI Image - Version 1.0
 # Image manipulation tool using ImageMagick
